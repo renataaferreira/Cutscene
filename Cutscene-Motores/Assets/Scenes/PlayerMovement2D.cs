e728c07941f61ac320a727d36dc5520ea6bbf107
renataaferreira <ferreira.renata@escolar.ifrn.edu.br>
2026-02-06 09:41:34 -0300

    ola
    oi

--- Cutscene-Motores/Assets/Scenes/PlayerMovement2D.cs
+++ Cutscene-Motores/Assets/Scenes/PlayerMovement2D.cs
@@ -4,29 +4,45 @@ public class PlayerMovement2D : MonoBehaviour
 {
     [Header("Movimento")]
     public float speed = 5f;
+    public float jumpForce = 8f;
+
+    [Header("Chão")]
+    public Transform groundCheck;
+    public LayerMask groundLayer;
+    public float groundRadius = 0.2f;
 
     private Animator anim;
     private SpriteRenderer sr;
+    private Rigidbody2D rb;
+
+    private bool isGrounded;
 
     void Awake()
     {
         anim = GetComponent<Animator>();
         sr = GetComponent<SpriteRenderer>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
         // =========================
-        // MOVIMENTO
+        // CHECAR CHÃO
         // =========================
-        float h = Input.GetAxisRaw("Horizontal");
-
-        transform.Translate(
-            new Vector3(h * speed * Time.deltaTime, 0f, 0f)
+        isGrounded = Physics2D.OverlapCircle(
+            groundCheck.position,
+            groundRadius,
+            groundLayer
         );
 
         // =========================
-        // FLIP (SEM MEXER NA ESCALA)
+        // MOVIMENTO
+        // =========================
+        float h = Input.GetAxisRaw("Horizontal");
+        rb.linearVelocity = new Vector2(h * speed, rb.linearVelocity.y);
+
+        // =========================
+        // FLIP
         // =========================
         if (h > 0)
             sr.flipX = false;
@@ -34,17 +50,36 @@ public class PlayerMovement2D : MonoBehaviour
             sr.flipX = true;
 
         // =========================
-        // ANIMAÇÃO DE ANDAR
+        // ANIMAÇÕES
         // =========================
         anim.SetBool("isRunning", h != 0);
+        anim.SetBool("isGrounded", isGrounded);
+
+        // =========================
+        // PULO
+        // =========================
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+            anim.SetTrigger("Jump");
+        }
 
         // =========================
         // ATAQUE
         // =========================
         if (Input.GetKeyDown(KeyCode.X))
         {
-            anim.ResetTrigger("Attack"); // segurança
             anim.SetTrigger("Attack");
         }
     }
+
+    void OnDrawGizmos()
+    {
+        if (groundCheck != null)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(groundCheck.position, groundRadius);
+        }
+    }
 }
+
